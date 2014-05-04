d93047bae255b748241fa7e4163a7165688ed144
Oleg Razgulyaev <oraz@mail.ru>
2014-05-04 19:16:37 +0400

    remove y2

--- imbalanced.py
+++ imbalanced.py
@@ -8,7 +8,7 @@
 import numpy as np
 import random
 
-def round_down(Xall,y1,y2):
+def round_down(Xall,y1):
     p_zeros = [i for i,e in enumerate(y1) if e == 0]
     p_ones = [i for i,e in enumerate(y1) if e > 0]
     delta = len(p_zeros) - len(p_ones)
@@ -19,12 +19,11 @@ def round_down(Xall,y1,y2):
         sel = random.sample(p_ones,len(p_zeros))
         sel = sel + p_zeros
     else:
-        return Xall,y1,y2
+        return Xall,y1
     print "round down:",len(p_zeros),len(p_ones),len(sel)
-    z2 = np.array([random.randint(0,1) for _ in range(len(sel))]).ravel()
-    return Xall[sel,:],y1[sel],z2
+    return Xall[sel,:],y1[sel]
 
-def round_up(Xall,y1,y2,ids):
+def round_up(Xall,y1,ids):
     ids_inv = [None]*Xall.shape[0]
     for (k,v) in ids.iteritems():
         for i in v:
@@ -38,21 +37,16 @@ def round_up(Xall,y1,y2,ids):
         delta = -delta
         sel = [random.choice(p_zeros) for _ in range(delta)]
     else:
-        return Xall,y1,y2,ids
+        return Xall,y1,ids
     X1 = [Xall]
     z1 = list(y1)
-    z2 = list(y2)
     j = Xall.shape[0]
     for i in sel:
         X1.append(Xall[i,:])
         z1.append(y1[i])
-        z2.append(y2[i])
         ids[ids_inv[i]].append(j)
         j += 1
     X1 = np.vstack(X1)
     z1 = np.array(z1).ravel()
-    #z2 = np.array(z2).ravel()
-    z2 = np.array([random.randint(0,1) for _ in range(len(z2))]).ravel()
-    print "round_up: 0:",len(p_zeros),"1:",len(p_ones),"X1:",X1.shape,"y1:",z1.shape,"y2:",z2.shape,"j_last:",j
-
-    return X1,z1,z2,ids
+    print "round_up: 0:",len(p_zeros),"1:",len(p_ones),"X1:",X1.shape,"y1:",z1.shape,"j_last:",j
+    return X1,z1,ids
