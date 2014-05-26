ff822de195735f3f3b6aed34213e0ff1d7c5a34d
Oleg Razgulyaev <oraz@mail.ru>
2014-05-26 22:18:59 +0400

    outliers.py

--- outliers.py
+++ outliers.py
@@ -9,7 +9,7 @@ import numpy as np
 from sklearn.covariance import EllipticEnvelope
 
 def search_outliers(X):
-    clf = EllipticEnvelope(contamination=0.01)
+    clf = EllipticEnvelope(contamination=0.5)
     clf.fit(X)
     is_outliers = clf.predict(X)
     return is_outliers
