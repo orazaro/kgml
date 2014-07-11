eb4a599c2672f5ed3425222ecf3da7043264a5e7
Oleg Razgulyaev <oraz@mail.ru>
2014-07-11 13:50:31 +0400

    outliers

--- outliers.py
+++ outliers.py
@@ -8,9 +8,44 @@
 import numpy as np
 from sklearn.covariance import EllipticEnvelope
 
-def search_outliers(X):
+def search_outliers_EllipticEnvelope(X):
     clf = EllipticEnvelope(contamination=0.2)
     clf.fit(X)
     is_outliers = clf.predict(X)
     return is_outliers
 
+def search_outliers_array(data, m = 6.):
+    """ http://stackoverflow.com/questions/11686720/is-there-a-numpy-builtin-to-reject-outliers-from-a-list """
+    d = np.abs(data - np.median(data))
+    mdev = np.median(d)
+    s = d/mdev if mdev else 0
+    return s>m
+
+def search_outliers(X,verbose=1):
+    n,m = X.shape
+    good = np.array([True] * n)
+    for j in range(m):
+        isout = search_outliers_array(X[:,j])
+        if np.any(isout):
+            bad = np.where(isout)[0]
+            good[bad] = False
+            if verbose>0:
+                print("outliers col:%d row_vals:%r"%(j,zip(bad,X[bad,j])))
+    return good
+
+
+def test():
+    data = np.arange(100)/100.
+    data[50] = 3.
+    print np.any(search_outliers_array(data))
+    X = np.array([range(20),range(20),range(20)])/20.
+    X[1,10] = 3.
+    X[1,14] = 5.
+    X[0,1] = 33.
+    #print search_outliers(X)
+    good = search_outliers(X.T)
+    print "good rows:",sum(good)
+    print X.T[good,:]
+
+if __name__ == "__main__":
+    test()
