76362bf396face8cb5007d0c8c90e484a72f14fd
Oleg Razgulyaev <oraz@mail.ru>
2014-07-13 20:40:40 +0400

    outliers

--- outliers.py
+++ outliers.py
@@ -77,9 +77,9 @@ def test():
     X[1,14] = 15.
     X[0,1] = 33.
     #print search_outliers(X)
-    good = search_outliers(X.T)
-    print "good rows:",sum(good)
-    #print X.T[good,:]
+    bad = search_outliers(X.T,m=4,mode=1)
+    print "bad rows:",sum(bad)
+    print X.T[bad,:]
 
 if __name__ == "__main__":
     test()
