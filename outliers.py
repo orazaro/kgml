1627638f290b0f82a3fb5064f1a4edcec442c6d4
Oleg Razgulyaev <oraz@mail.ru>
2014-07-14 12:30:52 +0400

    outliers

--- outliers.py
+++ outliers.py
@@ -59,9 +59,11 @@ def search_outliers(X, m = 6., mode = 1, verbose=1):
             ("pca", decomposition.RandomizedPCA(n_components=20, whiten=True,random_state=1))
             ]  ;
         X1 = Pipeline(pline).fit_transform(X)
-        print "X1:",X1.shape,X1[:,0]
-        outliers = X1[:,0]
-        sel_outliers = s_o_a(outliers,m=m)
+        print "X1:",X1.shape,X1[:,:4]
+        sel_outliers = np.array([False] * nrows)
+        for j in range(4):
+            outliers = X1[:,0]
+            sel_outliers = sel_outliers | s_o_a(outliers,m=m)
     else:
         raise ValueError("bad search_outliers mode: %r"%mode)
     if verbose>0:
