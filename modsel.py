91d09762dcfaefdd3a2d6daea15e045ce5fbfc15
Oleg Razgulyaev <oraz@mail.ru>
2015-03-03 14:01:32 +0300

    multiclass_log_loss

--- modsel.py
+++ modsel.py
@@ -8,6 +8,34 @@ from sklearn import cross_validation
 from sklearn.metrics import accuracy_score, mean_absolute_error, make_scorer
 from sklearn.metrics import roc_curve, auc
 
+
+def multiclass_log_loss(y_true, y_pred, eps=1e-15):
+    """Multi class version of Logarithmic Loss metric.
+    https://www.kaggle.com/wiki/MultiClassLogLoss
+
+    Parameters
+    ----------
+    y_true : array, shape = [n_samples]
+            true class, intergers in [0, n_classes - 1)
+    y_pred : array, shape = [n_samples, n_classes]
+
+    Returns
+    -------
+    loss : float
+    """
+    predictions = np.clip(y_pred, eps, 1 - eps)
+
+    # normalize row sums to 1
+    predictions /= predictions.sum(axis=1)[:, np.newaxis]
+
+    actual = np.zeros(y_pred.shape)
+    n_samples = actual.shape[0]
+    actual[np.arange(n_samples), y_true.astype(int)] = 1
+    vectsum = np.sum(actual * np.log(predictions))
+    loss = -1.0 / n_samples * vectsum
+    return loss
+
+
 def calc_roc_auc(y_test,y_proba):
     fpr, tpr, thresholds = roc_curve(y_test, y_proba)
     roc_auc = auc(fpr, tpr)
