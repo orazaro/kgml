b66ae2b8cddbefca40ab8df7ce8d990aa4810089
Oleg Razgulyaev <oraz@mail.ru>
2015-07-06 16:10:57 +0300

    functional.py

---
+++ functional.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:   Oleg Razgulyaev 
+# License:  BSD 3 clause
+"""
+    functionality
+"""
+
+class FunctionalWrapper(object):
+    def __init__(self, data):
+        self.data = data
+    def map(self, function):
+        """Call `map` on the items in `data` using the provided `function`"""
+        return FunctionalWrapper(map(function, self.data))
+    def reduce(self, function):
+        """Call `reduce` on the items in `data` using the provided `function`"""
+        return reduce(function, self.data)
+    def filter(self, function):
+        """Call `filter` on the items in `data` using the provided `function`"""
+        return FunctionalWrapper(filter(function, self.data))
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.__dict__ == other.__dict__)
+    def __getattr__(self, name):  return getattr(self.data, name)
+    def __getitem__(self, k):  return self.data.__getitem__(k)
+    def __repr__(self):  return 'FunctionalWrapper({0})'.format(repr(self.data))
+    def __str__(self):  return 'FunctionalWrapper({0})'.format(str(self.data))
+
