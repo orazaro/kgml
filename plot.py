dec4a370adc92fee0419e0d98c3d5bce5949bae0
Oleg Razgulyaev <oraz@mail.ru>
2015-10-14 21:25:28 +0300

    plot_roc_crossval

--- plot.py
+++ plot.py
@@ -117,3 +117,46 @@ def plot_decision_boundary(clf, X, y, ax, sample_weight=None, names=None, title=
     ax.set_title(title)
     #ax.legend()
 
+
+def plot_roc_crossval(model, X, y, show_folds=False):
+    """ Run classifier with cross-validation and plot ROC curves
+        from http://goo.gl/NMhWvf
+    """
+    from sklearn.cross_validation import StratifiedKFold
+    from scipy import interp
+    from sklearn.metrics import roc_curve, auc
+    import matplotlib.pyplot as plt    
+
+    cv = StratifiedKFold(y, n_folds=6)
+
+    mean_tpr = 0.0
+    mean_fpr = np.linspace(0, 1, 100)
+    all_tpr = []
+
+    for i, (train, test) in enumerate(cv):
+        probas_ = model.fit(X[train], y[train]).predict_proba(X[test])
+        # Compute ROC curve and area the curve
+        fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+        mean_tpr += interp(mean_fpr, fpr, tpr)
+        mean_tpr[0] = 0.0
+        if show_folds:
+            roc_auc = auc(fpr, tpr)
+            plt.plot(fpr, tpr, lw=1, label='ROC fold %d (area = %0.2f)' % (i, roc_auc))
+
+    plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')
+
+    mean_tpr /= len(cv)
+    mean_tpr[-1] = 1.0
+    mean_auc = auc(mean_fpr, mean_tpr)
+    plt.plot(mean_fpr, mean_tpr, 'k--',
+             label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
+
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic example')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
