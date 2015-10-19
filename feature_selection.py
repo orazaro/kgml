9b4191761bd7c418d71f1bf4ad3fccac7c655971
Oleg Razgulyaev <oraz@mail.ru>
2015-10-19 21:01:42 +0300

    forward_selected

--- feature_selection.py
+++ feature_selection.py
@@ -66,7 +66,7 @@ def add_quadratic_features(df, predictors, rm_noninform=True):
 
 
 
-def forward_selected(data, response, nmax=16, verbosity=0):
+def forward_selected(data, response, selmax=16, verbosity=0):
     """Linear model designed by forward selection.
 
     Parameters:
@@ -120,7 +120,7 @@ def forward_selected(data, response, nmax=16, verbosity=0):
             current_score = best_new_score
         if verbosity > 0:
             print current_score, selected
-        nmax -= 1; if nmax <= 0: break
+        if len(selected) >= selmax: break
     formula = "{} ~ {} + 1".format(response,
                                    ' + '.join(selected))
     model = smf.ols(formula, data).fit()
