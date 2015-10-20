684c57fd38f35c6555a38d7c59d207b2f121834a
Oleg Razgulyaev <oraz@mail.ru>
2015-10-20 12:23:01 +0300

    plot_calibration_curve.py

--- plot_calibration_curve.py
+++ plot_calibration_curve.py
@@ -43,22 +43,145 @@ but not where it is transposed-sigmoid (e.g., Gaussian naive Bayes).
 """
 # Author: Alexandre Gramfort <alexandre.gramfort@telecom-paristech.fr>
 #         Jan Hendrik Metzen <jhm@informatik.uni-bremen.de>
+#         Oleg Razgulyaev <oleg@razgulyaev.com>
 # License: BSD Style.
 
+import numpy as np
 import matplotlib.pyplot as plt
-
-from sklearn import datasets
-from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import LinearSVC
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import (brier_score_loss, precision_score, recall_score,
-                             f1_score)
+from sklearn.metrics import (brier_score_loss, precision_score, recall_score, f1_score)
 from sklearn.calibration import CalibratedClassifierCV, calibration_curve
-from sklearn.cross_validation import train_test_split
 
 
-def plot_calibration_curve(X_train, X_test, y_train, y_test, y, est, name, fig_index, bins=10):
+def plot_calibration_curve_boot(X, y, est, name, bins=10, n_iter=8, n_jobs=1, fig_index=1):
+    """ Plot calibration curve for est w/o and with calibration. 
+        using bootstrap
+    """
+    import sklearn.cross_validation as cross_validation
+    from sklearn import (metrics, cross_validation)
+    from model_selection import cross_val_predict_proba
+    
+    # Calibrated with isotonic calibration
+    cv = 2
+    isotonic = CalibratedClassifierCV(est, cv=cv, method='isotonic')
+
+    # Calibrated with sigmoid calibration
+    sigmoid = CalibratedClassifierCV(est, cv=cv, method='sigmoid')
+
+    fig = plt.figure(fig_index, figsize=(10, 10))
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 1), (2, 0))
+
+    ax1.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+    for clf, name in [(est, name),
+                      (isotonic, name + ' + Isotonic'),
+                      (sigmoid, name + ' + Sigmoid')]:
+        
+        for i_iter in range(n_iter):
+            N = X.shape[0]
+            train = np.random.choice(N,N)
+            train_i = set(train)
+            test = list(set(np.arange(N)) - set(train))
+            X_train, y_train  = X[train],y[train]
+            X_test, y_test = X[test],y[test]
+            
+            clf.fit(X_train, y_train)
+            y_pred = clf.predict(X_test)
+            if hasattr(clf, "predict_proba"):
+                y_proba = clf.predict_proba(X_test)[:, 1]
+            else:  # use decision function
+                prob_pos = clf.decision_function(X_test)
+                y_proba = \
+                    (prob_pos - prob_pos.min()) / (prob_pos.max() - prob_pos.min())
+        
+            
+        if False:
+            y_pred = np.array(y_proba>0.5,dtype=int)
+
+            clf_score = brier_score_loss(y_true, y_proba, pos_label=y_true.max())
+            print("%s:" % name)
+            print("\tBrier: %1.3f" % (clf_score))
+            print("\tPrecision: %1.3f" % precision_score(y_true, y_pred))
+            print("\tRecall: %1.3f" % recall_score(y_true, y_pred))
+            print("\tF1: %1.3f\n" % f1_score(y_true, y_pred))
+
+        fraction_of_positives, mean_predicted_value = \
+            calibration_curve(y_true, y_proba, n_bins=bins)
+
+        ax1.plot(mean_predicted_value, fraction_of_positives, "s-",
+                 label="%s (%1.3f)" % (name, clf_score))
+
+        ax2.hist(y_proba, range=(0, 1), bins=bins, label=name,
+                 histtype="step", lw=2)
+
+    ax1.set_ylabel("Fraction of positives")
+    ax1.set_ylim([-0.05, 1.05])
+    ax1.legend(loc="lower right")
+    ax1.set_title('Calibration plots  (reliability curve)')
+
+    ax2.set_xlabel("Mean predicted value")
+    ax2.set_ylabel("Count")
+    ax2.legend(loc="upper center", ncol=2)
+
+    plt.tight_layout()
+def plot_calibration_curve_cv(X, y, est, name, bins=10, n_folds=8, n_jobs=1, fig_index=1):
     """Plot calibration curve for est w/o and with calibration. """
+    import sklearn.cross_validation as cross_validation
+    from sklearn import (metrics, cross_validation)
+    from model_selection import cross_val_predict_proba
+    
+    # Calibrated with isotonic calibration
+    cv = 2
+    isotonic = CalibratedClassifierCV(est, cv=cv, method='isotonic')
+
+    # Calibrated with sigmoid calibration
+    sigmoid = CalibratedClassifierCV(est, cv=cv, method='sigmoid')
+
+    fig = plt.figure(fig_index, figsize=(10, 10))
+    ax1 = plt.subplot2grid((3, 1), (0, 0), rowspan=2)
+    ax2 = plt.subplot2grid((3, 1), (2, 0))
+
+    ax1.plot([0, 1], [0, 1], "k:", label="Perfectly calibrated")
+    for clf, name in [(est, name),
+                      (isotonic, name + ' + Isotonic'),
+                      (sigmoid, name + ' + Sigmoid')]:
+        
+        y_true = y
+        scoring = 'roc_auc'
+        cv1 = cross_validation.StratifiedKFold(y,n_folds)
+        y_proba, scores = cross_val_predict_proba(clf, X, y, scoring=scoring, 
+            cv=cv1, n_jobs=n_jobs, verbose=0, fit_params=None, pre_dispatch='2*n_jobs')
+        y_pred = np.array(y_proba>0.5,dtype=int)
+
+        clf_score = brier_score_loss(y_true, y_proba, pos_label=y_true.max())
+        print("%s:" % name)
+        print("\tBrier: %1.3f" % (clf_score))
+        print("\tPrecision: %1.3f" % precision_score(y_true, y_pred))
+        print("\tRecall: %1.3f" % recall_score(y_true, y_pred))
+        print("\tF1: %1.3f\n" % f1_score(y_true, y_pred))
+
+        fraction_of_positives, mean_predicted_value = \
+            calibration_curve(y_true, y_proba, n_bins=bins)
+
+        ax1.plot(mean_predicted_value, fraction_of_positives, "s-",
+                 label="%s (%1.3f)" % (name, clf_score))
+
+        ax2.hist(y_proba, range=(0, 1), bins=bins, label=name,
+                 histtype="step", lw=2)
+
+    ax1.set_ylabel("Fraction of positives")
+    ax1.set_ylim([-0.05, 1.05])
+    ax1.legend(loc="lower right")
+    ax1.set_title('Calibration plots  (reliability curve)')
+
+    ax2.set_xlabel("Mean predicted value")
+    ax2.set_ylabel("Count")
+    ax2.legend(loc="upper center", ncol=2)
+
+    plt.tight_layout()
+
+def plot_calibration_curve_old(X_train, X_test, y_train, y_test, y, est, name, fig_index, bins=10):
+    """Plot calibration curve for est w/o and with calibration. """
+    from sklearn.linear_model import LogisticRegression
     # Calibrated with isotonic calibration
     isotonic = CalibratedClassifierCV(est, cv=2, method='isotonic')
 
@@ -113,23 +236,37 @@ def plot_calibration_curve(X_train, X_test, y_train, y_test, y, est, name, fig_i
 
     plt.tight_layout()
 
-if __name__ == '__main__':
-    print(__doc__)
+def test_plot_calibration_curve():
+    from sklearn import datasets
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.svm import LinearSVC
+    from sklearn.cross_validation import train_test_split
+    X, y = datasets.make_classification(n_samples=100000, n_features=20,
+                                        n_informative=2, n_redundant=10,
+                                        random_state=42)
+    # Plot calibration cuve for Gaussian Naive Bayes
+    plot_calibration_curve(X, y, GaussianNB(), "Naive Bayes", fig_index=1 )
+    plt.show()
 
+def test_plot_calibration_curve_old():
+    from sklearn import datasets
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.svm import LinearSVC
+    from sklearn.cross_validation import train_test_split
+    print(__doc__)
     # Create dataset of classification task with many redundant and few
     # informative features
     X, y = datasets.make_classification(n_samples=100000, n_features=20,
                                         n_informative=2, n_redundant=10,
                                         random_state=42)
-
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.99,
                                                         random_state=42)
-
-
     # Plot calibration cuve for Gaussian Naive Bayes
-    plot_calibration_curve(GaussianNB(), "Naive Bayes", 1)
-
+    plot_calibration_curve_old(X_train, X_test, y_train, y_test, y, GaussianNB(), "Naive Bayes", 1)
     # Plot calibration cuve for Linear SVC
-    plot_calibration_curve(LinearSVC(), "SVC", 2)
-
+    plot_calibration_curve_old(X_train, X_test, y_train, y_test, y, LinearSVC(), "SVC", 2)
     plt.show()
+
+if __name__ == '__main__':
+    #test_plot_calibration_curve_old()
+    test_plot_calibration_curve()
