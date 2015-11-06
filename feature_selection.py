7c6b6ec5832c1008c7189ee5bc8b7afa19a8021a
Oleg Razgulyaev <oraz@mail.ru>
2015-11-06 12:53:26 +0300

    forward_cv

--- feature_selection.py
+++ feature_selection.py
@@ -65,23 +65,87 @@ def add_quadratic_features(df, predictors, rm_noninform=True):
     return df_out 
 
 
+def forward_cv(model, df, response, selmax=16, verbosity=0):
+    """ Forward selection using model.
 
-def forward_selected(data, response, selmax=16, verbosity=0):
-    """Linear model designed by forward selection.
+    Parameters
+    ----------
+    model:
+    df : DataFrame 
+        with all possible predictors and response
+    response: str 
+        name of response column in data
 
-    Parameters:
-    -----------
-    data : pandas DataFrame with all possible predictors and response
-
-    response: string, name of response column in data
-
-    Returns:
-    --------
+    Returns
+    -------
     model: an "optimal" fitted statsmodels linear model
            with an intercept
            selected by forward selection
            evaluated by adjusted R-squared
+    selected: list
+        selected predictors
+    Example:
+    --------
+    >> import pandas as pd
+    >> url = "http://data.princeton.edu/wws509/datasets/salary.dat"
+    >> data = pd.read_csv(url, sep='\\s+')
+    >> model = forward_selected(data, 'sl')
 
+    >> print model.model.formula
+    >> # sl ~ rk + yr + 1
+
+    >> print model.rsquared_adj
+    >> # 0.835190760538
+    
+    References:
+    -----------
+    http://planspace.org/20150423-forward_selection_with_statsmodels/
+
+    """
+    import statsmodels.formula.api as smf
+    remaining = set(data.columns)
+    remaining.remove(response)
+    selected = []
+    current_score, best_new_score = 0.0, 0.0
+    while remaining and current_score == best_new_score:
+        scores_with_candidates = []
+        for candidate in remaining:
+            formula = "{} ~ {} + 1".format(response,
+                                           ' + '.join(selected + [candidate]))
+            score = smf.ols(formula, data).fit().rsquared_adj
+            scores_with_candidates.append((score, candidate))
+        scores_with_candidates.sort()
+        best_new_score, best_candidate = scores_with_candidates.pop()
+        if current_score < best_new_score:
+            remaining.remove(best_candidate)
+            selected.append(best_candidate)
+            current_score = best_new_score
+        if verbosity > 0:
+            print current_score, selected
+        if len(selected) >= selmax: break
+    formula = "{} ~ {} + 1".format(response,
+                                   ' + '.join(selected))
+    model = smf.ols(formula, data).fit()
+    return model, selected
+
+def forward_selected(data, response, selmax=16, verbosity=0):
+    """Linear model designed by forward selection.
+
+    Parameters
+    ----------
+    data : pandas DataFrame 
+        with all possible predictors and response
+    response: str 
+        name of response column in data
+
+    Returns
+    -------
+    model: an "optimal" fitted statsmodels linear model
+           with an intercept
+           selected by forward selection
+           evaluated by adjusted R-squared
+    selected: list
+        selected predictors
     Example:
     --------
     >> import pandas as pd
