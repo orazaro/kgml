1674489ebfb525fccc4282ce12e263cf5be646ad
Oleg Razgulyaev <oraz@mail.ru>
2016-02-17 22:46:30 +0300

    refactor

--- classifier.py
+++ classifier.py
@@ -694,86 +694,97 @@ class RidgeCV_proba(lm.RidgeCV):
 
 
 class KNeighborsClassifier_proba(KNeighborsClassifier):
-    def predict_proba(self,X):
+    def predict_proba(self, X):
         y = super(KNeighborsClassifier_proba, self).predict_proba(X)
-        y[np.isnan(y)]=0.5
+        y[np.isnan(y)] = 0.5
         return y
 
+
 class SVC_proba(svm.SVC):
-  def predict_proba(self,X):
-    if hasattr(self, "decision_function"):  # use decision function
-        prob_pos = self.decision_function(X)
-        y = (prob_pos - prob_pos.min()) / (prob_pos.max() - prob_pos.min())
-        return np.vstack((1-y,y)).T
-    else:
-        raise RuntimeError("svm.SVC without decision_function")
+    def predict_proba(self, X):
+        if hasattr(self, "decision_function"):  # use decision function
+            prob_pos = self.decision_function(X)
+            y = (prob_pos - prob_pos.min()) / (prob_pos.max() - prob_pos.min())
+            return np.vstack((1-y, y)).T
+        else:
+            raise RuntimeError("svm.SVC without decision_function")
+
 
 class ConstClassifier(BaseEstimator, ClassifierMixin):
-  def __init__(self, c = 0):
-    self.c = c
-  def fit(self, X, y=None):
-    return self
-  def predict_proba(self, X):
-    X = np.asarray(X)
-    y1=np.empty(X.shape[0]); 
-    y1.fill(self.c)
-    y_proba = np.vstack((1-y1,y1)).T
-    return y_proba
-  def predict(self, X):
-    return self.predict_proba(X)[:,1]
+    def __init__(self, c=0):
+        self.c = c
+
+    def fit(self, X, y=None):
+        return self
+
+    def predict_proba(self, X):
+        X = np.asarray(X)
+        y1 = np.empty(X.shape[0])
+        y1.fill(self.c)
+        y_proba = np.vstack((1-y1, y1)).T
+        return y_proba
+
+    def predict(self, X):
+        return self.predict_proba(X)[:, 1]
+
 
 class MeanClassifier(BaseEstimator, ClassifierMixin):
-  def fit(self, X, y=None):
-    return self
-  def predict_proba(self, X):
-    X = np.asarray(X)
-    y1 = np.mean(X, axis=1)
-    y_proba = np.vstack((1-y1,y1)).T
-    return y_proba
-  def predict(self, X):
-    return self.predect_proba()[:,1]
-        
+    def fit(self, X, y=None):
+        return self
+
+    def predict_proba(self, X):
+        X = np.asarray(X)
+        y1 = np.mean(X, axis=1)
+        y_proba = np.vstack((1-y1, y1)).T
+        return y_proba
+
+    def predict(self, X):
+        return self.predect_proba()[:, 1]
+
+
 class RoundClassifier(BaseEstimator, ClassifierMixin):
-  """
-    Classifier with rounding classes 
-  """
-  def __init__(self, est, rup=0, find_cutoff=False):
-    self.est = est
-    self.rup = rup
-    self.find_cutoff = find_cutoff
+    """
+    Classifier with rounding classes
+    """
+    def __init__(self, est, rup=0, find_cutoff=False):
+        self.est = est
+        self.rup = rup
+        self.find_cutoff = find_cutoff
 
-  def fit(self, X, y):
-    from imbalanced import find_best_cutoff,round_smote,round_down,round_up
-    if self.rup > 0:
-        X1,y1,_ = round_up(X,y) 
-    elif self.rup < 0:
-        if self.rup < -1:
-            X1,y1 = round_smote(X,y) 
+    def fit(self, X, y):
+        from imbalanced import (find_best_cutoff, round_smote,
+                                round_down, round_up)
+        if self.rup > 0:
+            X1, y1, _ = round_up(X, y)
+        elif self.rup < 0:
+            if self.rup < -1:
+                X1, y1 = round_smote(X, y)
+            else:
+                X1, y1, _ = round_down(X, y)
         else:
-            X1,y1,_ = round_down(X,y) 
-    else:
-        X1,y1 = X,y
-    self.est.fit(X1,y1)
-    if self.find_cutoff:
-        ypp = self.predict_proba(X)[:,1]
-        self.cutoff = find_best_cutoff(y,ypp)
-    else:
-        self.cutoff = 0.5
-    return self
+            X1, y1 = X, y
+        self.est.fit(X1, y1)
+        if self.find_cutoff:
+            ypp = self.predict_proba(X)[:, 1]
+            self.cutoff = find_best_cutoff(y, ypp)
+        else:
+            self.cutoff = 0.5
+        return self
 
-  def predict_proba(self, X):
-    X = np.asarray(X)
-    if hasattr(self.est,'predict_proba'):
-        y_proba = self.est.predict_proba(X)
-    else:
-        y = self.est.predict(X)
-        y_proba = np.vstack((1-y,y)).T
-    return y_proba
-  def predict(self, X):
-    if not self.find_cutoff and hasattr(self.est,'predict'):
-        return self.est.predict(X)
-    ypp = self.predict_proba(X)[:,1]
-    return  np.array(map(int,ypp>self.cutoff))
+    def predict_proba(self, X):
+        X = np.asarray(X)
+        if hasattr(self.est, 'predict_proba'):
+            y_proba = self.est.predict_proba(X)
+        else:
+            y = self.est.predict(X)
+            y_proba = np.vstack((1-y, y)).T
+        return y_proba
+
+    def predict(self, X):
+        if not self.find_cutoff and hasattr(self.est, 'predict'):
+            return self.est.predict(X)
+        ypp = self.predict_proba(X)[:, 1]
+        return np.array(map(int, ypp > self.cutoff))
 
 
 def test():
@@ -785,10 +796,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Tuner.')
     parser.add_argument('cmd', nargs='?', default='test')
     args = parser.parse_args()
-    print(args, file=sys.stderr) 
-  
+    print(args, file=sys.stderr)
+
     if args.cmd == 'test':
         test()
     else:
         raise ValueError("bad cmd")
-
