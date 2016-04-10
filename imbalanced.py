0628063dd5f75a037210e65f4192c2bd8ed1cb8f
Oleg Razgulyaev <oraz@mail.ru>
2016-04-10 12:51:58 +0300

    imbalanced.py

--- imbalanced.py
+++ imbalanced.py
@@ -1,78 +1,111 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author:   Oleg Razgulyaev 
+# Author:   Oleg Razgulyaev
 # License:  BSD 3 clause
 """
-    Imbalanced datasets    
+    Imbalanced datasets
 """
 import numpy as np
 import random
 
 from collections import defaultdict
 from sklearn import metrics
+from smote import SMOTE
 
-def find_best_cutoff(y1,ypp,verbose=0):
+
+def balance_x_train(x_train, y_train):
+    """ Balance classes in the train dataset.
+    """
+    sel_train = np.arange(len(y_train))
+    r = np.unique(y_train, return_counts=True)
+    k = np.min(r[1])
+    sel = []
+    for a in r[0]:
+        sel_train_a = sel_train[y_train == a]
+        sel.extend(random.sample(sel_train_a, k))
+    return sel
+
+
+def test_balance_x_train():
+    x_train = np.arange(25)
+    y_train = (x_train % 3 == 0).astype(int)
+    r = np.unique(y_train, return_counts=True)
+    print(r)
+    assert r[1][0] != r[1][1]
+    sel = balance_x_train(x_train, y_train)
+    r = np.unique(y_train[sel], return_counts=True)
+    print(r)
+    assert r[1][0] == r[1][1]
+
+
+def find_best_cutoff(y1, ypp, verbose=0):
     from scipy import optimize
-    def f(x,*params):
-        y_true,ypp = params
-        y_pred = np.array(map(int,ypp>x))
+
+    def f(x, *params):
+        y_true, ypp = params
+        y_pred = np.array(map(int, ypp > x))
         res = metrics.f1_score(y_true, y_pred)
-        #print "x:",x,"res:",res
+        # print "x:",x,"res:",res
         return -res
-    rranges = (slice(0,1,0.01),)
-    resbrute = optimize.brute(f, rranges, args=(y1,ypp), full_output=False,
-                                  finish=optimize.fmin)
-    if verbose: print "resbrute:",resbrute
+    rranges = (slice(0, 1, 0.01),)
+    resbrute = optimize.brute(f, rranges, args=(y1, ypp), full_output=False,
+                              finish=optimize.fmin)
+    if verbose:
+        print "resbrute:", resbrute
     return resbrute[0]
 
-def ids_invert(X,ids):
+
+def ids_invert(X, ids):
     """ invert ids
         ids: dictionary of relation id -> rows in X
     """
     ids_inv = [None]*X.shape[0]
-    for (k,v) in ids.iteritems():
+    for (k, v) in ids.iteritems():
         for i in v:
             ids_inv[i] = k
     return ids_inv
 
+
 def round_down_sel(y1):
-    p_zeros = [i for i,e in enumerate(y1) if e <= 0]
-    p_ones = [i for i,e in enumerate(y1) if e > 0]
+    p_zeros = [i for i, e in enumerate(y1) if e <= 0]
+    p_ones = [i for i, e in enumerate(y1) if e > 0]
     delta = len(p_zeros) - len(p_ones)
     if delta > 0:
-        sel = random.sample(p_zeros,len(p_ones))
+        sel = random.sample(p_zeros, len(p_ones))
         sel = sel + p_ones
     elif delta < 0:
-        sel = random.sample(p_ones,len(p_zeros))
+        sel = random.sample(p_ones, len(p_zeros))
         sel = sel + p_zeros
     else:
         sel = None
     return sel
 
-def round_down(Xall,y1,ids=None):
+
+def round_down(Xall, y1, ids=None):
     sel = round_down_sel(y1)
     if sel is None:
-        return Xall,y1,ids
-    #print "round down:",len(p_zeros),len(p_ones),len(sel)
+        return Xall, y1, ids
+    # print "round down:",len(p_zeros),len(p_ones),len(sel)
     if ids is not None:
-        ids_inv = ids_invert(Xall,ids)
+        ids_inv = ids_invert(Xall, ids)
         ids2 = defaultdict(list)
-        for (j,i) in enumerate(sel):
+        for (j, i) in enumerate(sel):
             ids2[ids_inv[i]].append(j)
-        #print len(sel),sum([len(ids[k]) for k in ids])
+        # print len(sel),sum([len(ids[k]) for k in ids])
         # renumerate ids2 as range(0,len(ids2))
         ids = defaultdict(list)
-        for i,id2 in enumerate(ids2):
+        for i, id2 in enumerate(ids2):
             ids[i] = ids2[id2]
-        return Xall[sel,:],y1[sel],ids
+        return Xall[sel, :], y1[sel], ids
     else:
-        return Xall[sel,:],y1[sel],ids
+        return Xall[sel, :], y1[sel], ids
 
-def round_up(Xall,y1,ids=None):
-    if ids is not None: 
-        ids_inv = ids_invert(Xall,ids)
-    p_zeros = [i for i,e in enumerate(y1) if e == 0]
-    p_ones = [i for i,e in enumerate(y1) if e > 0]
+
+def round_up(Xall, y1, ids=None):
+    if ids is not None:
+        ids_inv = ids_invert(Xall, ids)
+    p_zeros = [i for i, e in enumerate(y1) if e == 0]
+    p_ones = [i for i, e in enumerate(y1) if e > 0]
     delta = len(p_zeros) - len(p_ones)
     if delta > 0:
         sel = [random.choice(p_ones) for _ in range(delta)]
@@ -80,44 +113,43 @@ def round_up(Xall,y1,ids=None):
         delta = -delta
         sel = [random.choice(p_zeros) for _ in range(delta)]
     else:
-        return Xall,y1,ids
+        return Xall, y1, ids
     X1 = [Xall]
     z1 = list(y1)
     j = Xall.shape[0]
     for i in sel:
-        X1.append(Xall[i,:])
+        X1.append(Xall[i, :])
         z1.append(y1[i])
         if ids is not None:
             ids[ids_inv[i]].append(j)
         j += 1
     X1 = np.vstack(X1)
     z1 = np.array(z1).ravel()
-    #print "round_up: 0:",len(p_zeros),"1:",len(p_ones),"X1:",X1.shape,"y1:",z1.shape,"j_last:",j
-    return X1,z1,ids
+    # print "round_up: 0:",len(p_zeros),"1:",len(p_ones),"X1:",X1.shape,
+    # "y1:",z1.shape,"j_last:",j
+    return X1, z1, ids
 
-from smote import SMOTE
 
-def round_smote(Xall,y1,k=5,h=1.0):
-    p_zeros = [i for i,e in enumerate(y1) if e == 0]
-    p_ones = [i for i,e in enumerate(y1) if e > 0]
+def round_smote(Xall, y1, k=5, h=1.0):
+    p_zeros = [i for i, e in enumerate(y1) if e == 0]
+    p_ones = [i for i, e in enumerate(y1) if e > 0]
     delta = len(p_zeros) - len(p_ones)
     if delta > 0:
-        N = ( int(len(p_zeros)/len(p_ones))+1 ) * 100
-        T = Xall[p_ones,:]
+        N = (int(len(p_zeros)/len(p_ones))+1) * 100
+        T = Xall[p_ones, :]
         S = SMOTE(T, N, k, h)
-        sel = random.sample(range(S.shape[0]),delta)
-        X1 = np.vstack([Xall,S[sel,:]])
-        z1 = np.hstack([y1,np.ones(delta)])
+        sel = random.sample(range(S.shape[0]), delta)
+        X1 = np.vstack([Xall, S[sel, :]])
+        z1 = np.hstack([y1, np.ones(delta)])
     elif delta < 0:
         delta = -delta
-        N = ( int(len(p_ones)/len(p_zeros))+1 ) * 100
-        T = Xall[p_zeros,:]
+        N = (int(len(p_ones)/len(p_zeros))+1) * 100
+        T = Xall[p_zeros, :]
         S = SMOTE(T, N, k, h)
-        sel = random.sample(range(S.shape[0]),delta)
-        X1 = np.vstack([Xall,S[sel,:]])
-        z1 = np.hstack([y1,np.zeros(delta)])
+        sel = random.sample(range(S.shape[0]), delta)
+        X1 = np.vstack([Xall, S[sel, :]])
+        z1 = np.hstack([y1, np.zeros(delta)])
     else:
-        return Xall,y1
-    #print "round smote:","X1:",X1.shape,"z1:",z1.shape
-    return X1,z1
-
+        return Xall, y1
+    # print "round smote:","X1:",X1.shape,"z1:",z1.shape
+    return X1, z1
