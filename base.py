1486b7109acd7c4100da3ddeb497cce4012c3a5d
Oleg Razgulyaev <oraz@mail.ru>
2016-05-01 11:26:05 +0300

    np_shift

--- base.py
+++ base.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# Author:   Oleg Razgulyaev 
+# Author:   Oleg Razgulyaev
 # License:  BSD 3 clause
 """
     base obects and functions
 """
 
-import platform;
+import platform
 if platform.python_implementation() == 'PyPy':
     import numpypy as np
     import numpypy_extend
+    numpypy_extend  # unflask
 else:
     import numpy as np
+
 
 class Bunch(dict):
     """Container object for datasets: dictionary-like object that
@@ -21,17 +23,22 @@ class Bunch(dict):
         dict.__init__(self, kwargs)
         self.__dict__ = self
 
+
 def mean_absolute_error(y_true, y_pred):
     return np.mean(np.abs(y_pred - y_true))
 
-def t_round_equal(x,y,n = 5):
-    return round(abs(x-y),n) == 0.0
+
+def t_round_equal(x, y, n=5):
+    return round(abs(x-y), n) == 0.0
+
 
 def test():
-    b = Bunch(x=2,y=3)
+    b = Bunch(x=2, y=3)
     print b
     assert str(b) == "{'y': 3, 'x': 2}"
-    assert mean_absolute_error(np.array([1.,2.3]),np.array([-2.1,3.3])) == 2.05
+    assert mean_absolute_error(np.array([1., 2.3]),
+                               np.array([-2.1, 3.3])) == 2.05
+
 
 def check_n_jobs(n_jobs):
     import multiprocessing
@@ -40,3 +47,13 @@ def check_n_jobs(n_jobs):
     else:
         return n_jobs
 
+
+def np_shift(xs, n):
+    e = np.empty_like(xs)
+    if n >= 0:
+        e[:n] = np.nan
+        e[n:] = xs[:-n]
+    else:
+        e[n:] = np.nan
+        e[:n] = xs[-n:]
+    return e
