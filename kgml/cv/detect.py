bec59eb8f68c02833b8699a33e457479a1c728d4
Oleg Razgulyaev <oraz@mail.ru>
2016-06-15 00:59:06 +0300

    HueSaturationTransformer

--- kgml/cv/detect.py
+++ kgml/cv/detect.py
@@ -38,22 +38,92 @@ class HueSaturationTransformer(BaseEstimator, TransformerMixin):
     def __init__(self):
         pass
 
-    def fit(self, X, y=None, shape=None):
-        self.y = y
-        self.shape = shape
+    def fit(self, X, Y=None):
+        self.Y = Y
+        if Y is None:
+            return
+        assert X.shape == Y.shape
+        img_shape = X.shape
+        X = rgb_to_hsv(X)
+        X = X.reshape((-1, 3))[:, 0]
+        y = Y.reshape((-1, 3))[:, 0]
+        
+        self.bins = 10
+        
+        X_pool = X[y.astype(bool)]
+        hist_1, bin_edges_1 = np.histogram(X_pool, range=(0, 1),
+                                         bins=self.bins,
+                                         density=True)
+        hist_1 /= self.bins
+        X_nopool = X[np.logical_not(y.astype(bool))]
+        hist_0, bin_edges_0 = np.histogram(X_nopool, range=(0, 1),
+                                         bins=self.bins,
+                                         density=True)
+        hist_0 /= self.bins
+        assert tuple(bin_edges_1) == tuple(bin_edges_0)
+        print(len(bin_edges_1), len(hist_1))
+        print(zip(bin_edges_1, hist_0, hist_1))
+
+        if False:
+            fig, ax = plt.subplots()
+            bin_centers = 0.5*(bin_edges_1[:-1] + bin_edges_1[1:])
+            ax.plot(bin_centers, hist_1, lw=2, color='blue', label='pool')
+            ax.plot(bin_centers, hist_0, lw=2, color='red', label='nopool')
+            ax.set_xlim((0, 1))
+            ax.set_ylim((0, 1))
+            plt.legend()
+            plt.show()
+
+        self.bin_edges = bin_edges_1
+        n_hist = len(hist_1)
+        assert len(hist_0) == n_hist
+        sels = np.zeros(n_hist, dtype=int)
+        for i in range(n_hist):
+            if hist_1[i] > 0.1:
+                if hist_0[i] <= 0.01:
+                    sels[i] = 1
+                elif hist_1[i] / hist_0[i] > 2.0:
+                    sels[i] = 1
+        self.sels = sels
+
+        return self
+        
+    def transform_hs(self, X):
+        hue_min=0.45 
+        hue_max=0.60
+        satur_min=0.4
+        # select using hue
+        img = X
+        hue = rgb_to_hsv(img)[:, :, 0]
+        img = img.copy()
+        img[hue < hue_min] = 0
+        img[hue > hue_max] = 0
+
+        # select usin saturation
+        img = rgb_to_hsv(img)[:, :, 1]
+        binary_img = img > satur_min
+
+        # Remove small white regions
+        open_img = ndimage.binary_opening(binary_img)
+        # Remove small black hole
+        close_img = ndimage.binary_closing(open_img)
+
+        return close_img
 
     def transform(self, X):
-        if self.y is None:
+        if self.Y is None:
             return transform_hs(X)
+        else:
+            return self.transform_hs(X)
 
 
-def create_test_image(x1=10, y1=20, d=10):
+def create_test_image(x1=10, y1=20, d=10, c=0.55):
     """Create test image with blue rect inside."""
     shape = (32, 64, 3)
     img = np.full(shape, 0.5)
     img[:, :30, 0] = 0.10
     img[:, 30:, 0] = 0.90
-    img[y1:y1+d, x1:x1+d, 0] = 0.55
+    img[y1:y1+d, x1:x1+d, 0] = c
     img = hsv_to_rgb(img)
     return img
 
@@ -81,22 +151,43 @@ def show_patch(ax, p, color='black'):
         )
 
 
-def test_HueSaturationTransformer():
+def zest_HueSaturationTransformer(c=0.35):
     """Create image and transform it."""
     p1 = (10, 10, 10)
     p_train = (8, 8, 14)
     p2 = (40, 10, 10)
-    img_train = create_test_image(x1=p1[0], y1=p1[1], d=p1[2])
-    img_test = create_test_image(x1=p2[0], y1=p2[1], d=p2[2])
-    fig, axarr = plt.subplots(3, 1)
+    img_train = create_test_image(x1=p1[0], y1=p1[1], d=p1[2], c=c)
+    img_test = create_test_image(x1=p2[0], y1=p2[1], d=p2[2], c=c)
+
+    Y = np.zeros(img_train.shape)
+    x1, x2 = p_train[0], p_train[0] + p_train[2]
+    y1, y2 = p_train[1], p_train[1] + p_train[2]
+    Y[y1:y2+1, x1:x2+1] = 1
+    hst = HueSaturationTransformer()
+    hst.fit(img_train, Y)
+    img_pred = hst.transform(img_test)
+
+    #return
+
+    fig, axarr = plt.subplots(4, 1)
     ax = axarr[0]
     ax.imshow(img_train, cmap='gray')
     show_patch(ax, p_train)
     axarr[1].imshow(img_test, cmap='gray')
-    img = transform_hs(img_test)
-    axarr[2].imshow(img, cmap='gray')
+    img_hs = transform_hs(img_test)
+    axarr[2].imshow(img_hs, cmap='gray')
+    axarr[3].imshow(img_pred, cmap='gray')
     plt.show()
+
+
+def test_HueSaturationTransformer():
+    zest_HueSaturationTransformer(c=0.55)
+
+
+def test_HueSaturationTransformer2():
+    zest_HueSaturationTransformer(c=0.35)
 
 if __name__ == '__main__':
     # test_transform_hs()
     test_HueSaturationTransformer()
+    # test_HueSaturationTransformer2()
