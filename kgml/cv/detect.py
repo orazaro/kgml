640cbb0f7ee662e24dd03dc39c6bfcb9377b3248
Oleg Razgulyaev <oraz@mail.ru>
2016-06-14 12:15:40 +0300

    object detector

---
+++ kgml/cv/detect.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author:   Oleg Razgulyaev
+# License:  BSD 3 clause
+"""
+    Object Detector
+"""
+from __future__ import division, print_function
+from sklearn.base import BaseEstimator, TransformerMixin
+
+
+class HueSatTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
