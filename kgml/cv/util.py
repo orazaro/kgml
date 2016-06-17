f7e62172d78bf80c598573ee8dad2bb2871c59f5
Oleg Razgulyaev <oraz@mail.ru>
2016-06-17 16:28:57 +0300

    hsv to rgb

--- kgml/cv/util.py
+++ kgml/cv/util.py
@@ -11,40 +11,51 @@ import matplotlib.pyplot as plt
 import cv2
 
 
-def img_norm(img, is_hsv=False):
+def img_norm(img, is_hsv=False, back=False):
     divs = np.array([255., 255., 255.])
     if is_hsv:
         divs[0] = 179.
-    img = img.astype(np.float32)
-    for i in range(3):
-        img[:, :, i] = img[:, :, i] / divs[i]
-    return img
+    if back:
+        for i in range(3):
+                img[:, :, i] = img[:, :, i] * divs[i]
+        return img.astype(np.uint8)
+    else:
+        img = img.astype(np.float32)
+        for i in range(3):
+                img[:, :, i] = img[:, :, i] / divs[i]
+        return img
 
 
-def rgb_to_hsv(img, back=False):
-    from_to = cv2.COLOR_HSV2RGB if back else cv2.COLOR_RGB2HSV
+def rgb_to_hsv(img):
     if len(img.shape) == 3:
-        img2 = cv2.cvtColor(img, from_to)
+        img2 = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     else:
         img_shape = img.shape
         img3 = img[np.newaxis, :, :]
-        img2 = cv2.cvtColor(img3, from_to).reshape(img_shape)
-    
+        img2 = cv2.cvtColor(img3, cv2.COLOR_RGB2HSV).reshape(img_shape)
     return img_norm(img2, is_hsv=True)
 
 
 def hsv_to_rgb(img):
-    return rgb_to_hsv(img, back=True)
+    img = img_norm(img, is_hsv=True, back=True)
+    if len(img.shape) == 3:
+        img2 = cv2.cvtColor(img, cv2.COLOR_HSV2RGB)
+    else:
+        img_shape = img.shape
+        img3 = img[np.newaxis, :, :]
+        img2 = cv2.cvtColor(img3, cv2.COLOR_HSV2RGB).reshape(img_shape)
+    return img2
 
 
 def test_rgb_to_hsv():
-    img = np.zeros((1, 2, 3), dtype=np.float32)
+    img = np.zeros((2, 2, 3), dtype=np.float32)
     img[:, :] = np.array([10, 30, 150], dtype=np.float32)
     img = img / 255.
-    print(img)
-    img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
-    print(img_hsv)
-    print(rgb_to_hsv(img))
+    print("rgb:\n", img)
+    img_hsv = rgb_to_hsv(img)
+    print("hsv:\n", img_hsv)
+    img2 = hsv_to_rgb(img_hsv)
+    print("rgb2:\n", img2)
     
     # plt.imshow(img)
     # plt.show()
