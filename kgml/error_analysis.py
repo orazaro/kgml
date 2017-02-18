cfc85ee3ef75d43dfa355dc11f33e7b0bd9c7966
Oleg Razgulyaev <oraz@mail.ru>
2017-02-18 21:36:17 +0300

    hinge_loss_array

--- kgml/error_analysis.py
+++ kgml/error_analysis.py
@@ -11,13 +11,13 @@ import random
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.metrics import confusion_matrix
-from sklearn.preprocessing import LabelBinarizer
+from sklearn.metrics import confusion_matrix, hinge_loss
+from sklearn.preprocessing import LabelBinarizer, LabelEncoder
 from sklearn.datasets import make_classification
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import cross_val_predict
-from sklearn.utils import check_array, check_consistent_length
+from sklearn.utils import check_array, check_consistent_length, column_or_1d
 
 
 RANDOM_STATE = 1
@@ -178,9 +178,108 @@ def test_log_loss_array():
     print("lla:", lla.mean(), lla)
 
 
+def hinge_loss_array(y_true, pred_decision, labels=None):
+    """hinge loss (non-regularized)
+    Copied from https://goo.gl/uy3oFZ
+    In binary class case, assuming labels in y_true are encoded with +1 and -1,
+    when a prediction mistake is made, ``margin = y_true * pred_decision`` is
+    always negative (since the signs disagree), implying ``1 - margin`` is
+    always greater than 1.  The cumulated hinge loss is therefore an upper
+    bound of the number of mistakes made by the classifier.
+    In multiclass case, the function expects that either all the labels are
+    included in y_true or an optional labels argument is provided which
+    contains all the labels. The multilabel margin is calculated according
+    to Crammer-Singer's method. As in the binary case, the cumulated hinge loss
+    is an upper bound of the number of mistakes made by the classifier.
+    Read more in the :ref:`User Guide <hinge_loss>`.
+    Parameters
+    ----------
+    y_true : array, shape = [n_samples]
+        True target, consisting of integers of two values. The positive label
+        must be greater than the negative label.
+    pred_decision : array, shape = [n_samples] or [n_samples, n_classes]
+        Predicted decisions, as output by decision_function (floats).
+    labels : array, optional, default None
+        Contains all the labels for the problem. Used in multiclass hinge loss.
+    sample_weight : array-like of shape = [n_samples], optional
+        Sample weights.
+    Returns
+    -------
+    loss : float
+    References
+    ----------
+    .. [1] `Wikipedia entry on the Hinge loss
+           <https://en.wikipedia.org/wiki/Hinge_loss>`_
+    .. [2] Koby Crammer, Yoram Singer. On the Algorithmic
+           Implementation of Multiclass Kernel-based Vector
+           Machines. Journal of Machine Learning Research 2,
+           (2001), 265-292
+    .. [3] `L1 AND L2 Regularization for Multiclass Hinge Loss Models
+           by Robert C. Moore, John DeNero.
+           <http://www.ttic.edu/sigml/symposium2011/papers/
+           Moore+DeNero_Regularization.pdf>`_
+    """
+    check_consistent_length(y_true, pred_decision)
+    pred_decision = check_array(pred_decision, ensure_2d=False)
+    y_true = column_or_1d(y_true)
+    y_true_unique = np.unique(y_true)
+    if y_true_unique.size > 2:
+        if (labels is None and pred_decision.ndim > 1 and
+                (np.size(y_true_unique) != pred_decision.shape[1])):
+            raise ValueError("Please include all labels in y_true "
+                             "or pass labels as third argument")
+        if labels is None:
+            labels = y_true_unique
+        le = LabelEncoder()
+        le.fit(labels)
+        y_true = le.transform(y_true)
+        mask = np.ones_like(pred_decision, dtype=bool)
+        mask[np.arange(y_true.shape[0]), y_true] = False
+        margin = pred_decision[~mask]
+        margin -= np.max(pred_decision[mask].reshape(y_true.shape[0], -1),
+                         axis=1)
+
+    else:
+        # Handles binary class case
+        # this code assumes that positive and negative labels
+        # are encoded as +1 and -1 respectively
+        pred_decision = column_or_1d(pred_decision)
+        pred_decision = np.ravel(pred_decision)
+
+        lbin = LabelBinarizer(neg_label=-1)
+        y_true = lbin.fit_transform(y_true)[:, 0]
+
+        try:
+            margin = y_true * pred_decision
+        except TypeError:
+            raise TypeError("pred_decision should be an array of floats.")
+
+    losses = 1 - margin
+    # The hinge_loss doesn't penalize good enough predictions.
+    losses[losses <= 0] = 0
+    return losses
+
+
+def test_hinge_loss_array(n_classes=9):
+    X, y = make_classification(
+        n_samples=10000, n_features=20, n_informative=10, n_redundant=2,
+        n_repeated=0, n_classes=n_classes, n_clusters_per_class=2)
+    clf = LogisticRegression()
+    y_pred_proba = cross_val_predict(clf, X, y, cv=4, n_jobs=1,
+                                     method='predict_proba')
+
+    if n_classes == 2:
+        y_pred_proba = y_pred_proba[:, 1]
+    lla = hinge_loss_array(y, y_pred_proba, labels=None)
+    print("hinge_loss:", hinge_loss(y, y_pred_proba))
+    print("lla:", lla.mean(), lla)
+
+
 def test(args):
     # test_get_confusion_matrix()
-    test_log_loss_array()
+    # test_log_loss_array()
+    # test_hinge_loss_array()
+    test_hinge_loss_array(n_classes=2)
     print("Test OK", file=sys.stderr)
 
 if __name__ == '__main__':
